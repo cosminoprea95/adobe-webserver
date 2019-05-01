64daa40a07b0dcaa66d6d20ffa7fd639b488d4fe
Cosmin Oprea <cosminoprea995@gmail.com>
2019-05-01 22:38:17 +0300

    Add Main class and resource loading configuration

---
+++ src/main/java/exception/ConfigurationException.java
@@ -0,0 +1,7 @@
+package exception;
+
+/**
+ * Created by cosminoprea on 5/1/19.
+ */
+public class ConfigurationException extends Throwable {
+}
---
+++ src/main/java/handler/RequestHandler.java
@@ -0,0 +1,13 @@
+package handler;
+
+/**
+ * Created by cosminoprea on 5/1/19.
+ */
+public class RequestHandler implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+    
+}
